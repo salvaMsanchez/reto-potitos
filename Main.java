1c9448b74bb4768faebc1c2632acd87266475494
Salva Moreno <morenosanchezsalva@gmail.com>
2023-07-29 23:56:51 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,92 @@
+package salva.saliendoCrisis;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    static java.util.Scanner in;
+
+    public static boolean casoDePrueba() {
+
+        in = new java.util.Scanner(System.in);
+
+        int numPotitos = -1;
+        Boolean rangoMuestras = false;
+        do {
+            int numPotitosInterno = in.nextInt();
+            in.nextLine();
+            if (0 < numPotitosInterno && numPotitosInterno <= 25) {
+                rangoMuestras = true;
+                numPotitos = numPotitosInterno;
+            } else if (numPotitosInterno == 0) {
+                rangoMuestras = true;
+                numPotitos = numPotitosInterno;
+            }
+        } while (!rangoMuestras);
+
+        if (numPotitos == 0) {
+            return false;
+        } else {
+
+            Boolean finPotitos = false;
+            int contador = 0;
+            ArrayList<String> ingredientesSi = new ArrayList<>();
+            ArrayList<String> ingredientesNo = new ArrayList<>();
+            ArrayList<String> ingredientesOdiados = new ArrayList<>();
+            while (!finPotitos) {
+
+                if (contador != numPotitos) {
+                    String potito = in.nextLine();
+                    potito = potito.trim();
+                    potito = potito.replaceAll("\\s+", " ");
+
+                    String[] ingredientesSucio = potito.split("\\s+");
+                    if (ingredientesSucio[0].equals("SI:")) {
+                        for (int i = 0; i < ingredientesSucio.length; i++) {
+                            if (!ingredientesSucio[i].equals("SI:") && !ingredientesSucio[i].equals("NO:") && !ingredientesSucio[i].equals("FIN")) {
+                                ingredientesSi.add(ingredientesSucio[i]);
+                            }
+                        }
+                    } else if (ingredientesSucio[0].equals("NO:")) {
+                        for (int i = 0; i < ingredientesSucio.length; i++) {
+                            if (!ingredientesSucio[i].equals("SI:") && !ingredientesSucio[i].equals("NO:") && !ingredientesSucio[i].equals("FIN")) {
+                                ingredientesNo.add(ingredientesSucio[i]);
+                            }
+                        }
+                    }
+
+                    contador++;
+
+                } else {
+                    finPotitos = true;
+                }
+
+            }
+
+            for (String ingredienteNo: ingredientesNo) {
+                if (!ingredientesSi.contains(ingredienteNo)) {
+                    ingredientesOdiados.add(ingredienteNo);
+                }
+            }
+
+            // Ordenar alfabéticamente ArrayList
+            Collections.sort(ingredientesOdiados);
+
+            for (String ingredienteOdiado : ingredientesOdiados) {
+                System.out.print(ingredienteOdiado + " ");
+            }
+            System.out.println();
+
+            return true;
+        }
+
+    }
+
+    public static void main(String args[]) {
+        in = new java.util.Scanner(System.in);
+        while (casoDePrueba()) {
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Potitos
+
+![Entrada del programa](images/tiempoMemoria.png)
+
+"¡Javi! ¡Parece que éste tampoco le gusta!" se convirtió en cantinela habitual a la hora de la comida, antes de que el pequeño dejara definitivamente de comer potitos una tarde. Darle de comer era misión imposible; pese a la paciencia de sus padres, el niño había heredado el carácter cabezón del padre y mantenía la boca cerrada, inmune a los vuelos de la cucharita y a las monerías de quienes le rodeaban. Era necesario encontrar una solución.
+
+Se les ocurrió que, quizá, el aparentemente caprichoso rechazo a los potitos se debía a algún ingrediente concreto. Comenzaron a anotar, cuidadosamente, todos los ingredientes de cada potito que le daban, junto con un comentario de si el pequeño se lo había tomado o no. Tras varios días, estaban convencidos de que podrían averiguar cuáles eran los ingredientes que no le gustaban, y así comprar aquellos potitos que fuera a comerse. ¿Puedes ayudarles a encontrarlos?
+
+## Entrada
+
+La entrada estará compuesta de múltiples casos de prueba. Cada uno comienza con un número indicando el número de potitos que se han intentado dar al bebé (como máximo 25).
+
+A continuación aparece una línea por cada potito. La línea comienza por SI: o NO: dependiendo de si el pequeño se comió o no el potito. Después aparece la lista de los ingredientes del potito separados por espacios. La lista se cierra con la palabra FIN, que no debe considerarse un ingrediente. Ningún potito tiene más de 10 ingredientes, y todos los ingredientes están compuestos por una única palabra de hasta 20 letras minúsculas.
+
+La entrada acaba con un caso de prueba sin potitos.
+
+## Salida
+
+Para cada caso de prueba se deben mostrar, en una línea, los ingredientes que no le gustan al niño, ordenados alfabéticamente y separados por espacio. Si todos los ingredientes le gustan, se dejará la linea en blanco.
+
+## Entrada de ejemplo
+
+![Entrada del programa](images/entrada.png)
+
+## Salida de ejemplo
+
+![Salida del programa](images/salida.png)
+
+## Lenguaje empleado
+
+<p align="center">
+	<a href="https://docs.oracle.com/en/java/index.html">
+	    <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" alt="Git">
+	</a>
+</p>
+
+---
+
+[🛜 Aquí puedes encontrar el sitio web oficial donde se encuentra este reto.](https://aceptaelreto.com/pub/problems/v001/85/st/statements/Spanish/index.html)
